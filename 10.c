37fdf297b378d4f0a03020f1f2f51163542ee825
anshikabansal223-cyber <anshikabansal223@gmail.com>
2026-06-03 19:09:25 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,22 @@
+//WAP to print prime numbers in a range
+#include<stdio.h>
+int main(){
+    int start,end,i,j,count;
+    printf("Enter the starting number:");
+    scanf("%d",&start);
+    printf("Enter the ending number:");
+    scanf("%d",&end);
+    printf("The prime numbers are:\n");
+    for(i=start;i<=end;i++){
+        count=0;
+        for(j=1;j<=i;j++){
+            if(i%j==0){
+                count++;
+            }
+        }
+        if(count ==2){
+            printf("%d",i);
+        }
+    }
+    return 0;
+}
