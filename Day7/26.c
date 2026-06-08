9f2e54c9b7e52aeae27973c22681c631cf655d41
anshikabansal223-cyber <anshikabansal223@gmail.com>
2026-06-08 22:41:43 +0530

    Update 26.c

--- Day7/26.c
+++ Day7/26.c
@@ -1,18 +1,25 @@
-//WAP to recursive factorial
-#include<Stdio.h>
-long long factorial(int n){
-    (if n==0 || n==1){
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if(n == 0)
+        return 0;
+    else if(n == 1)
         return 1;
-    }
-    else{
-        return n*factorial(n-1);
-    }
-    
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
-int main(){
-    int n;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    printf("Factorial of %d =%lld\n",n,factorial(n))
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+
+    for(i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
     return 0;
 }
