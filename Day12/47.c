15dd639f5d8110e72790b870ae9dad8a782c4085
anshikabansal223-cyber <anshikabansal223@gmail.com>
2026-06-26 08:29:44 +0530

    Create 47.c

---
+++ Day12/47.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void fibonacci (int n);
+int main(){
+    int n;
+    printf("Enter no of terms:");
+    scanf("%d",&n);
+    fibonacci(n);
+    return 0;
+}
+void fibonacci (int n){
+    int a=0,b=1,c,i;
+    for(i=1;i<=n;i++){
+        printf("%d",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
