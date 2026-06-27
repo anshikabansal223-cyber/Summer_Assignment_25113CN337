50f5817a361707e49fd67f2596ad4ba9fbf103de
anshikabansal223-cyber <anshikabansal223@gmail.com>
2026-06-27 20:38:21 +0530

    Create 63.c

---
+++ Day16/63.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, j, sum;
+    int found = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the required sum: ");
+    scanf("%d", &sum);
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == sum) {
+                printf("Pair found: %d and %d\n", arr[i], arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if(found)
+            break;
+    }
+    if(found == 0)
+        printf("No pair found.");
+
+    return 0;
+}
