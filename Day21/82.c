ab68c6ce492eea1041f35e2ab014590af2f1d338
anshikabansal223-cyber <anshikabansal223@gmail.com>
2026-06-30 21:49:53 +0530

    Create 82.c

---
+++ Day21/82.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    char str[100], rev[100];
+    int i, length = 0;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    while(str[length] != '\0')
+    {
+        length++;
+    }
+    for(i = 0; i < length; i++)
+    {
+        rev[i] = str[length - i - 1];
+    }
+    rev[i] = '\0';
+    printf("Reversed String = %s", rev);
+    return 0;
+}
