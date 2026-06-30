fa2cf623b1b13b71c2b70500504fb0cf415c74a0
anshikabansal223-cyber <anshikabansal223@gmail.com>
2026-06-30 10:32:43 +0530

    Create 73.c

---
+++ Day19/73.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],b[10][10],sum[10][10];
+    int rows, cols;
+    int i, j;
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &rows, &cols);
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("\nFirst Matrix is:\n");
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Enter elements of second matrix\n:");
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("\nSecond matrix is:\n");
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("\nSum of matrices:\n");
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            printf("%d ",sum[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
