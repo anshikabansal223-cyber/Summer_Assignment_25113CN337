78edead0c42190015469a46097f5becde2b422c9
anshikabansal223-cyber <anshikabansal223@gmail.com>
2026-06-30 21:46:28 +0530

    Create 81.c

---
+++ Day21/81.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    int i, length=0;
+    printf("Enter a string:");
+    scanf("%s",str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        length++;
+    }
+    printf("Length of string is %d",length);
+    return 0;
+}
