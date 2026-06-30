d18d4794166e642ade1e11314e8c9fc1d0967600
anshikabansal223-cyber <anshikabansal223@gmail.com>
2026-06-30 18:21:25 +0530

    Create 78.c

---
+++ Day20/78.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10];
+    int n, i, j,flag=1;
+    printf("Enter the order of square matrix:");
+    scanf("%d",&n);
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("\nFirst Matrix is:\n");
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(a[i][j]!= a[j][i]){
+                flag =0;
+                break;
+            }
+        }
+        if(flag==0){
+            break;
+        }
+    }
+    if(flag==1){
+        printf("The matrix is symmetric.");
+    }
+    else{
+        printf("The matrix is not symmetric.");
+    }
+    return 0;
+}
